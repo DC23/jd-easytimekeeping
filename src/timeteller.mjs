4b53027d3c8c536a0d72848333d2f53a594009c1
DC23 <DC23@users.noreply.github.com>
2025-04-13 17:31:59 +1000

    Implemented custom time chat message macro callback #218

--- src/timeteller.mjs
+++ src/timeteller.mjs
@@ -14,8 +14,24 @@ export class TimeTeller {
         TimeTeller.#checkAutoTellTime(data.time)
     }
 
-    static tellTime (time) {
-        const content = Helpers.toTimeString(time, true)
+    static async tellTime (time) {
+        /**
+         * if there is a macro registered in the
+         * module settings then we'll call it to format the time,
+         * otherwise use the built-in method
+         */
+        let content = null
+        if (game.user.isGM) {
+            const formatMacro = TimeTeller.#timeChatFormatMacro
+            if (formatMacro) {
+                content = await formatMacro.execute({ time: time, includeDay: true })
+            }
+        }
+
+        if (!content) {
+            content = Helpers.toTimeString(time, true)
+        }
+
         console.log('JD ETime | %s', content)
         ChatMessage.create({
             speaker: { actor: game.user.id },
@@ -27,5 +43,11 @@ export class TimeTeller {
         const tellTimeSettings = game.settings.get(MODULE_ID, SETTINGS.AUTO_TELL_TIME_SETTINGS)
         const timeOfDay = Helpers.toTimeOfDay(time, '12hour')
         if (tellTimeSettings[timeOfDay]) this.tellTime(time)
+    }
+
+    static get #timeChatFormatMacro () {
+        return game.macros.find(
+            m => m.uuid === game.settings.get(MODULE_ID, SETTINGS.TIME_CHAT_FORMAT_MACRO)
+        )
     }
 }
